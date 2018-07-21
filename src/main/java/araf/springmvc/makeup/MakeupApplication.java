f132c697fb5587d7819948e18a00c4f0643bd13f
araf488 <araf488@gmail.com>
2018-07-21 14:11:37 +0600

    config java-xml

--- src/main/java/araf/springmvc/makeup/MakeupApplication.java
+++ src/main/java/araf/springmvc/makeup/MakeupApplication.java
@@ -2,8 +2,10 @@ package araf.springmvc.makeup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class MakeupApplication {
 
     public static void main(String[] args) {
